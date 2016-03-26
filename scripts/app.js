8b9f7fda98617883b228f674e210acc8ba1ce38f
Magdalena Jadach <magdalena.jadach@gmail.com>
2016-03-26 19:44:32 +0000

    jQuery fetch data from API

--- scripts/app.js
+++ scripts/app.js
@@ -2,24 +2,37 @@
 
 // Write a series of JS instructions to create a structure as the "Flickrtatoes" app.
 
-$(document).ready(function() {
-var apiUrl = "https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json"; // My API request
-$.get(apiUrl, function(data) {
-    $('body').html("<h1>It works!</h1>");
-
-})
-})
-// 	var body = $('body');	
-// 	var newDiv = $('<div></div');
-
-// 	var newHeader = $('<h1>'Flickrtatoes'</h1>');
-// 	newHeader.appendTo(newDiv);
-
-
-
-// 	newDiv.appendTo(body);
-
+// $(document).ready(function() {
+// 	var apiUrl = "https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json"; // My API request
+// 	$.get(apiUrl, function(data) {
+// 	    $('body').html("<h1>It works!</h1>");
+// 	    var newDiv = $('<div></div');
+// 	    newDiv.attr('class', 'first');
+// 	    newDiv.appendTo(body);
+// 	})
 // })
+
+
+(function() {
+  var flickerAPI = 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+  $.getJSON(flickerAPI, {
+    tags: 'potato',
+    format: 'json'
+  })
+    .done(function( data ) {
+      $.each( data.items, function( i, item ) {
+        $( '<img>' ).attr( 'src', item.media.m ).appendTo( '#potato' );
+        // $('<p>').attr(item.title).appendTo('#potato');
+        $('<a>').attr('src', item.link).appendTo('#potato');
+        if ( i === 19 ) {
+          return false;
+        }
+      });
+    });
+})(); // his is example from http://api.jquery.com/jquery.getjson/ I try to add another elements from that place
+// like "title" or "url", but with no success
+
+
 
 // 1. <h1>Flickrtatoes sketch</h1>
 
